7080a38547658e6bf6390fb50668f244e0ae65a1
Johnathan James <haxwell@gmail.com>
2024-04-29 23:14:27 -0600

    Initial commit. Server listens and connects to mongodb atlas instance.

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PropertiesReader = require('properties-reader');
+const prop = PropertiesReader('_environments/application.properties');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+getProperty = (pty) => { return prop.get(pty);}
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Connect to MongoDB Atlas
+const dbURI = getProperty('mongo.connect.string');
+mongoose.connect(dbURI, { })
+  .then(() => console.log('Database connected'))
+  .catch(err => console.log('DB connection error:', err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
