21cfec4df794f58c5136950ed4738bcf33894397
PrakGov <55034062+prak112@users.noreply.github.com>
2024-05-28 16:44:49 +0300

    build: setup request routes, rename MongoDB URI variable

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 
-const PropertiesReader = require('properties-reader');
-const prop = PropertiesReader('.env');
-
+const loginRouter = require('./controllers/login')
+const signupRouter = require('./controllers/signup')
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-getProperty = (pty) => { return prop.get(pty);}
 
 // Middleware to parse JSON
 app.use(express.json());
 
+// Route requests
+app.use('/public/login', loginRouter);
+app.use('/public/signup', signupRouter);
+
 // Connect to MongoDB Atlas
-const dbURI = getProperty('mongo.connect.string');
-mongoose.connect(dbURI, { })
+const mongodb_uri = process.env.MONGODB_URI
+mongoose.connect(mongodb_uri, { })
   .then(() => console.log('Database connected'))
-  .catch(err => console.log('DB connection error:', err));
+  .catch(error => console.log('DB connection error:', error));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
